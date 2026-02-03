d26b07989588faf6e3bab62db8ec327f48587dbb
sarmad <sarmadraza208@gmail.com>
2026-02-03 15:31:07 +0500

    remove listen

--- db.js
+++ db.js
@@ -1,15 +1,41 @@
+// import mongoose from "mongoose";
+
+// export async function connectDB() {
+//   const uri = process.env.MONGO_URI;
+
+//   if (!uri) {
+//     throw new Error("MONGO_URI is missing");
+//   }
+
+//   await mongoose.connect(uri, {
+//     dbName: "sps_accounting"
+//   });
+
+//   console.log("✅ MongoDB connected");
+// }
+
+
 import mongoose from "mongoose";
 
-export async function connectDB() {
-  const uri = process.env.MONGO_URI;
+let cached = global.mongoose;
 
-  if (!uri) {
-    throw new Error("MONGO_URI is missing");
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  const uri = process.env.MONGO_URI;
+  if (!uri) throw new Error("MONGO_URI is missing");
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(uri, {
+      dbName: "sps_accounting",
+    });
   }
 
-  await mongoose.connect(uri, {
-    dbName: "sps_accounting"
-  });
-
+  cached.conn = await cached.promise;
   console.log("✅ MongoDB connected");
+  return cached.conn;
 }
